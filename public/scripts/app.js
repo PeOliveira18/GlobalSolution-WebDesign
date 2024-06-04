1cedb73b7643b2bc3a9d4329d7e9256ff82dc9c3
Pedro Oliveira <oliveirasilvapedro2@gmail.com>
2024-06-04 19:52:23 -0300

    Mudancas gerais

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -44,7 +44,7 @@ function realizarCadastro(){
     document.getElementById('usuario').value = ''
     document.getElementById('senha').value = ''
 
-    location.href = '../index.html';
+    location.href = '/index.html';
 }
 
 
@@ -59,7 +59,7 @@ function verificarLogin(){
         alert('Login efetuado com sucesso')
         document.getElementById('usuarioLogin').value = ''
         document.getElementById('senhaLogin').value = ''
-        location.href = '../index.html'
+        location.href = '/index.html'
     }else{
         alert('Nome de usuario ou senha incorretos. Tente novamente')
     } 
@@ -106,6 +106,7 @@ function verificarEmail(){
         
     }else if (!emailValido){
         alert('Voce precisa digitar um endereco de e-mail')
+        location.href = '#eMail'
         
     }else{
         alert('Digite um endereco de e-mail correto')
